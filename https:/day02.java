b203d1d0bd05e0afeb13c1e2466120d41458e18a
rahminmadani878-jpg <rahminmadani878@gmail.com>
2026-09-03 13:18:09 +0800

    Create day02.java

---
+++ https:/day02.java
@@ -0,0 +1,20 @@
+public class Main {
+	public static void main(String[] args) {
+		//menggunakan println()
+		System.out.println ("Biodata diri");
+		
+		System.out.println("Nama   =  Rahmin Madani");
+		System.out.println("Nim    =  D0226014");
+		System.out.println("Kelas  =  Informatika B");
+		
+		
+		//menggunakan print()
+		System.out.print("prodi  =  ");
+		System.out.print("informatika");
+		System.out.print("\n");
+		
+		//menggunakan printf()
+		String kota = "Kab.Majene";
+		int umur = 18;
+	}
+}
