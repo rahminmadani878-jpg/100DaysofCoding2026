bbb4fc23c52deef4f48cf9dc6f8a65f771c923f7
rahminmadani878-jpg <rahminmadani878@gmail.com>
2026-09-04 21:02:50 +0800

    Create Day03.java

---
+++ https:/Day03.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        //  Escape sequence \" Kutip Ganda
+        System.out.println("Penjual: \"Bakso sapi asli!\"");
+
+
+        //  Escape sequence \' Kutip Tunggal
+        System.out.println("Ini Bakso\' Pak prabowo ");
+
+
+        //  Escape sequence \\ Garis Miring Terbalik
+        System.out.println("Simpan resep di: D:\\Resep\\Bakso.txt");
+        
+
+        //  Escape sequence \n Baris Baru
+        System.out.println("\nDaftar Menu:\n1. Bakso Urat\n2. Bakso Telur");
+        
+
+        //  Escape sequence \t Tab (jarak rapi)
+        System.out.println("\tNama\t\tHarga");
+        System.out.println("\tUrat\t\tRp 15.000");
+        System.out.println("\tTelur\t\tRp 18.000");
+    }
+}
