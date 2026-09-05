3cd9ee2d1247899f7caa08dcb1b2ec0edcf3a481
rahminmadani878-jpg <rahminmadani878@gmail.com>
2026-09-05 17:06:05 +0800

    Create Day04.java

---
+++ https:/Day04.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+        // byte — untuk angka kecil
+        byte nilaiAtas = 6;
+        System.out.println("Kartu: " + nilaiAtas );
+
+        // short — untuk angka lebih besar dari byte
+        short poinSaya = 120;
+        System.out.println("Poin — Saya: " + poinSaya);
+
+        // int — angka bulat biasa tanpa tanda koma
+        int totalPoin = 2500;
+        System.out.println("Total Poin: " + totalPoin);
+
+        // long — untuk bilangan paling besar dan di akhir ada huruf L
+        long totalPermainan = 1250000L; 
+        System.out.println("Total Permainan: " + totalPermainan);
+	}
+}
