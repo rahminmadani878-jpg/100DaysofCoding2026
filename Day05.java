858e4c341291a37a388e23efa241a22a0a19c746
rahminmadani878-jpg <rahminmadani878@gmail.com>
2026-09-06 15:07:32 +0800

    Create Day05.java

---
+++ Day05.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+        // double → bilangan desimal tanpa huruf tambahan 
+        double beratPerKilo = 5.5;
+
+        // float → bilangan desimal dan wajib ada huruf F
+        float hargaPerKg = 5.5123F;
+
+        // Tampilkan
+        System.out.println("Berat per Kg : " + beratPerKilo + " kg");
+        
+        System.out.printf("Harga per kg : %.4f%n", hargaPerKg);
+	}
+}l
