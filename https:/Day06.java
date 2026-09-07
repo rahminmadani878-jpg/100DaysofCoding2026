2b21a3a525a021f999d4c71a7b53f00ae5f4269e
rahminmadani878-jpg <rahminmadani878@gmail.com>
2026-09-07 18:53:51 +0800

    Create Day06.java

---
+++ https:/Day06.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+        // char → 1 huruf, pakai kutip satu ' '
+        char jenisKelamin = 'L';
+
+        // boolean → hanya true atau false
+        boolean lulus = true;
+
+        // Tampilkan
+        System.out.println(" jenis Kelamin: " + jenisKelamin);
+        System.out.println("Lulus: " + lulus);
+	}
+}
