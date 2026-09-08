37ec36f22f62b1b32bc2edd67d768d102d20213b
rahminmadani878-jpg <rahminmadani878@gmail.com>
2026-09-08 19:02:34 +0800

    Create Day07.java

---
+++ https:/Day07.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+        // String — untuk teks wajib pakai tanda KUTIP DUA
+        String nama = "Zilong";
+        String tipe = "Fighter/Assassin";
+        String julukan = "Hero Epic";
+
+        // Tampilkan
+        System.out.println("Nama     : " + nama);
+        System.out.println("Tipe     : " + tipe);
+        System.out.println("Julukan  : " + julukan);
+	}
+}
