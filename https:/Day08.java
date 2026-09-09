59198e5911d4fb4886ec228bda5edf295e1d7f8c
rahminmadani878-jpg <rahminmadani878@gmail.com>
2026-09-09 19:32:06 +0800

    Create Day08.java

---
+++ https:/Day08.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+        // DEKLARASI —> siapkan variabel, belum ada nilai
+        int level;
+
+        //  INISIALISASI —> beri nilai PERTAMA
+        level = 1;
+        System.out.println("Sebelum diubah : " + level);
+
+        //  UPDATE —> ganti nilai dengan yang BARU
+        level = 100;
+        System.out.println("Sesudah diubah : " + level);
+	}
+}
