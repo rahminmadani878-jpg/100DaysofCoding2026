a5d3a7b478954d4de66e86fc396ae4752c9b7299
rahminmadani878-jpg <rahminmadani878@gmail.com>
2026-09-10 16:55:57 +0800

    Create Day09.java

---
+++ https:/Day09.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+        // Konstanta = nilai tetap  atau tidak bisa diubah (final)
+        final int DETIK_DALAM_MENIT = 60;
+        final int DETIK_DALAM_JAM = 3600;
+
+        System.out.println("1 Menit = " + DETIK_DALAM_MENIT + " detik");
+        System.out.println("1 Jam   = " + DETIK_DALAM_JAM + " detik");
+	}
+}
