03b08ca4e9458d5b4f6101dd29ef0499c1a822d9
rahminmadani878-jpg <rahminmadani878@gmail.com>
2026-09-15 20:45:03 +0800

    Create Day14.java

---
+++ https:/Day14.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main(String[] args) {
+        
+        // PENJUMLAHAN (+)
+        int angka1 = 20;
+        int angka2 = 10;
+        int hasilTambah = angka1 + angka2;
+        
+        //  PENGURANGAN (-)
+        int angka3 = 40;
+        int angka4 = 30;
+        int hasilKurang = angka3 - angka4;
+        
+        // MENAMPILKAN HASIL
+        System.out.println("Penjumlahan: " + angka1 + " + " + angka2 + " = " + hasilTambah);
+        System.out.println("Pengurangan: " + angka3 + " - " + angka4 + " = " + hasilKurang);
+	}
+}
