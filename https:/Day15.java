d150d9d84516d5ba56e39be78d60abe942527d14
rahminmadani878-jpg <rahminmadani878@gmail.com>
2026-09-16 21:25:18 +0800

    Create Day15.java

---
+++ https:/Day15.java
@@ -0,0 +1,20 @@
+public class Main {
+	public static void main(String[] args) {
+        
+        // Menentukan angka
+        int angka1 = 4;
+        int angka2 = 2;
+        
+        // Melakukan perhitungan
+        int hasilPerkalian = angka1 * angka2;
+        int hasilPembagian = angka1 / angka2;
+        
+        // Menampilkan hasil
+        System.out.println("Angka 1 : " + angka1);
+        System.out.println("Angka 2 : " + angka2);
+        
+        System.out.println("Perkalian : " + hasilPerkalian);
+        System.out.println("Pembagian : " + hasilPembagian);
+        
+    }
+}
