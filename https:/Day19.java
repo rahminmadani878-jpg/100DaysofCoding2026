9d61ebfe0aabb51386ae3b21f55305d7fb86e076
rahminmadani878-jpg <rahminmadani878@gmail.com>
2026-09-20 22:21:49 +0800

    Create Day19.java

---
+++ https:/Day19.java
@@ -0,0 +1,19 @@
+		import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan harga bakso per porsi: ");
+        double harga = input.nextDouble();
+
+        int  hargaBulat = (int) harga;
+        short hargaKecil = (short) hargaBulat;
+        byte hargaPalingKecil = (byte) hargaKecil;
+
+        System.out.println("\nHasil Konversi:");
+        System.out.println("Asli    = " + harga);
+        System.out.println("int     = " + hargaBulat);
+        System.out.println("byte    = " + hargaPalingKecil);
+	}
+}
